719924cd5e4f4f5bc6cebc93b95dad938402bb70
Alfredo Diaz <alfredo.diaz@therore.net>
2015-05-23 00:26:53 +0200

    v1.0.0

---
+++ zookeeperdump.groovy
@@ -0,0 +1,118 @@
+#!/usr/bin/env groovy
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import org.apache.commons.cli.HelpFormatter
+import org.apache.curator.framework.AuthInfo
+import org.apache.curator.framework.CuratorFrameworkFactory
+import org.apache.curator.retry.ExponentialBackoffRetry
+import org.apache.zookeeper.KeeperException
+import org.apache.zookeeper.data.ACL
+import org.apache.zookeeper.data.Id
+import org.springframework.beans.MutablePropertyValues
+import org.springframework.boot.bind.RelaxedDataBinder
+import org.yaml.snakeyaml.Yaml
+
+@Grab("org.yaml:snakeyaml:1.13")
+@Grab("org.apache.curator:curator-recipes:2.7.1")
+@Grab("org.slf4j:slf4j-api:1.7.6")
+@Grab("org.slf4j:slf4j-log4j12:1.7.11")
+@Grab("org.springframework:spring-context:4.1.6.RELEASE")
+@Grab("org.springframework.boot:spring-boot-autoconfigure:1.2.2.RELEASE")
+@Grab("com.fasterxml.jackson.core:jackson-databind:2.4.0")
+@Grab("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.4.0")
+
+def cli = new CliBuilder(width: 180, usage: '''
+zookeeperdump.groovy [-s {ip:port}] [-x | -c] [-u scheme:id] [-a {scheme:id:perm,scheme:id:perm...}] zpath
+examples:
+zookeeperdump.groovy -x /config/application > dump.yml
+zookeeperdump.groovy -c /config/application < dump.yml
+zookeeperdump.groovy -u digest:usr:pwd -a digest:usr:uPIxv8DxE/mT5RPGVrsDMJnQoTQ=:rw -c /config/application < dump.yml
+zookeeperdump.groovy -u digest:usr:pwd -a digest:usr:uPIxv8DxE/mT5RPGVrsDMJnQoTQ=:r -c /config/application < dump.yml
+zookeeperdump.groovy -u digest:super:secret  -c /config/myapplication < dump.yml
+zookeeperdump.groovy -u digest:super:secret  -x /config/myapplication > dump.yml
+'''.trim())
+cli.s(longOpt:'server', 'zookeeper server connection. By default localhost:2181', required:false, args:1)
+cli.x(longOpt:'extract', 'extract tree in yaml format to stdout', required:false, args:0)
+cli.c(longOpt:'create', 'import tree in yaml format from stdin', required:false, args:0)
+cli.u(longOpt:'auth', 'set session authentication', required:false, args:1)
+cli.a(longOpt:'acls', 'set acls used for creation', required:false, args:1)
+
+OptionAccessor opt = cli.parse(args)
+
+if( (!opt.x && !opt.c) || opt.arguments().size!=1) {
+    cli.usage()
+    return
+}
+
+def server = opt.s ?: "localhost:2181"
+def rootZPath = opt.arguments()[0]
+def auth = opt.u ? opt.u.split(",").collect{it.split(":")}.collect{new AuthInfo(it[0],it[1..-1].join(':').bytes)}:null
+def acls = opt.a ? opt.a.split(",").collect{it.split(":")}
+        .collect{new ACL(it[-1].chars.collect{1<<"rwcda".indexOf(it as String)}
+        .inject(0){r,i->r|i},new Id(it[0],it[1..-2].join(':')))} : null
+
+
+println "acls = $acls"
+
+
+def curatorBuilder = CuratorFrameworkFactory.builder()
+            .connectionTimeoutMs(1000)
+            .connectString(server)
+            .retryPolicy(new ExponentialBackoffRetry(1000, 5))
+
+if (auth)
+    curatorBuilder.authorization(auth)
+
+def curator = curatorBuilder.build()
+curator.start();
+
+/// EXPORT TREE TO YAML
+if (opt.x) {
+    new ObjectMapper(new YAMLFactory()).writeValue(System.out,
+        { zpath ->
+            zpath = zpath.startsWith('/')? zpath.substring(1):zpath
+            children = curator.children.forPath("/$zpath")
+            if (children.empty) {
+                (bytes = curator.data.forPath("/$zpath")) ? new String(bytes, "UTF-8") : ""
+            } else
+                children.inject([:]){ map, name -> map[name] = owner.call("$zpath/" + name); map }.sort{it.key}
+        }.call(rootZPath))
+}
+
+
+
+/// IMPORT TREE FROM YAML
+if (opt.c) {
+    map = [:]
+    yaml = new Yaml().load(System.in)
+    if (yaml instanceof Map)
+        new RelaxedDataBinder(map).bind(new MutablePropertyValues(yaml))
+    else
+        map = yaml;
+
+    rootZPath = rootZPath.startsWith('/')? rootZPath : "/$rootZPath"
+
+    try {curator.delete().deletingChildrenIfNeeded().forPath(rootZPath);} catch(KeeperException.NoNodeException e ){}
+
+    rootZPath.findIndexValues(1,{it=='/'}).collect{rootZPath[0..it-1]}.each{
+        builder = curator.create()
+        if (acls)
+            builder = builder.withACL(acls)
+        try {builder.forPath(it, null)}catch(KeeperException.NodeExistsException e){}
+    }
+
+    ({ zpath, content ->
+        builder = curator.create()
+        if (acls)
+            builder = builder.withACL(acls)
+        if (content instanceof Map) {
+            builder.forPath(zpath, null)
+            content.sort{it.key}.each {e -> owner.call("$zpath/" + e.key, e.value)}
+        } else {
+            builder.forPath(zpath, String.valueOf(content).getBytes());
+        }
+    }.call(rootZPath, map))
+
+}
+
+curator.close()
